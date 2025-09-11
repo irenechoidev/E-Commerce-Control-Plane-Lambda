91531fc629cffcf45205b184eba727a3adb8d3aa
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-09-11 16:17:45 -0600

    [AE-30] implement logic for createProductImage (#8)

--- src/main/java/ecommerce/config/AppConfig.java
+++ src/main/java/ecommerce/config/AppConfig.java
@@ -6,24 +6,23 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 public class AppConfig {
     private static final SdkHttpClient httpClient = UrlConnectionHttpClient.builder().build();
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private static final S3Presigner s3Presigner = S3Presigner.create();
     private static final S3Client s3Client = S3Client.builder()
             .httpClient(httpClient)
             .build();
-    private static final ECommerceS3Client ecommerceS3Client = new ECommerceS3Client(s3Client);
+
+    private static final ECommerceS3Client ecommerceS3Client = new ECommerceS3Client(s3Client, s3Presigner);
 
     public static PasswordEncoder getPasswordEncoder() {
         return passwordEncoder;
     }
-
     public static SdkHttpClient getHttpClient() {
         return httpClient;
     }
-
-    public static ECommerceS3Client getEcommerceS3Client() {
-        return ecommerceS3Client;
-    }
+    public static ECommerceS3Client getEcommerceS3Client() { return ecommerceS3Client; }
 }
--- src/main/java/ecommerce/dao/ProductDao.java
+++ src/main/java/ecommerce/dao/ProductDao.java
@@ -1,15 +1,21 @@
 package ecommerce.dao;
 
 import ecommerce.models.Product;
+import ecommerce.models.ProductImage;
 import lombok.NonNull;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 
 public class ProductDao {
     private final DynamoDbTable<Product> productTable;
+    private final DynamoDbTable<ProductImage> productImageTable;
 
-    public ProductDao(DynamoDbTable<Product> productTable) {
+    public ProductDao(
+            DynamoDbTable<Product> productTable,
+            DynamoDbTable<ProductImage> productImageTable
+    ) {
         this.productTable = productTable;
+        this.productImageTable = productImageTable;
     }
 
     public void createProduct(@NonNull Product product) {
@@ -21,5 +27,9 @@ public class ProductDao {
         return productTable.getItem(Key.builder()
                 .partitionValue(productId)
                 .build());
+    }
+
+    public void createProductImage(@NonNull ProductImage productImage) {
+        productImageTable.putItem(productImage);
     }
 }
