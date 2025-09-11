b0c2d85f93516354acfd5a2e6a06f17d697b9121
irenechoidev <143298470+irenechoidev@users.noreply.github.com>
2025-09-11 11:23:02 -0600

    [NO-TICKET] move dyanmodb db client configuration logic to a config file (#6)

--- src/main/java/ecommerce/dao/ProductDao.java
+++ src/main/java/ecommerce/dao/ProductDao.java
@@ -2,29 +2,14 @@ package ecommerce.dao;
 
 import ecommerce.models.Product;
 import lombok.NonNull;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class ProductDao {
-    private static final String TABLE_NAME = "e-commerce-product-table-v1";
-    private static final TableSchema<Product> productSchema = TableSchema.fromBean(Product.class);
-    private static DynamoDbEnhancedClient enhancedClient;
-    private static DynamoDbTable<Product> productTable;
+    private final DynamoDbTable<Product> productTable;
 
-    public ProductDao() {
-        DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
-                .httpClient(UrlConnectionHttpClient.builder().build())
-                .build();
-
-        enhancedClient = DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
-                .build();
-
-        productTable = enhancedClient.table(TABLE_NAME, productSchema);
+    public ProductDao(DynamoDbTable<Product> productTable) {
+        this.productTable = productTable;
     }
 
     public void createProduct(@NonNull Product product) {
--- src/main/java/ecommerce/dao/UserDao.java
+++ src/main/java/ecommerce/dao/UserDao.java
@@ -1,34 +1,18 @@
 package ecommerce.dao;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ecommerce.models.User;
 import lombok.NonNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class UserDao {
-    private static final String TABLE_NAME = "e-commerce-user-table-v1";
-    private static final TableSchema<User> userSchema = TableSchema.fromBean(User.class);
-    private static DynamoDbEnhancedClient enhancedClient;
-    private static PasswordEncoder passwordEncoder;
-    private static DynamoDbTable<User> userTable;
+    private final PasswordEncoder passwordEncoder;
+    private final DynamoDbTable<User> userTable;
 
-    public UserDao() {
-        DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
-                .httpClient(UrlConnectionHttpClient.builder().build())
-                .build();
-
-        enhancedClient = DynamoDbEnhancedClient.builder()
-                .dynamoDbClient(dynamoDbClient)
-                .build();
-
-        userTable = enhancedClient.table(TABLE_NAME, userSchema);
-        passwordEncoder = new BCryptPasswordEncoder();
+    public UserDao(DynamoDbTable<User> userTable, PasswordEncoder passwordEncoder) {
+        this.userTable = userTable;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createUser(@NonNull User user) {
